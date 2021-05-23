cbc72de7202d2518343fff692cacfe0ed39ffa2d
Heliostat9 <heliostat@inbox.ru>
2021-05-23 19:42:59 +0300

    test system

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react'
+import Test from './components/test';
 function App() {
+  const [type, setType] = useState('Выберите тип');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <form style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '30%'
+        }} method="POST" action="http://localhost:3000/content" encType="multipart/form-data"> 
+          <input name="title" type="text" placeholder="Введите название"/>
+          <textarea name="desc" placeholder="Введите описание">
+          </textarea>
+          <select name="category" defaultValue="Выберите категорию">
+            <option disabled>Выберите категорию</option>
+            <option value="Теория множеств">Теория множеств</option>
+            <option value="Линейная алгебра">Линейная алгебра</option>
+          </select>
+          <select name="type" value={type} onChange={(e) => setType(e.target.value)}>
+            <option disabled value="Выберите тип">Выберите тип</option>
+            <option value="Лекция">Лекция</option>
+            <option value="Тест">Тест</option>
+            <option value="Практическая">Практическая</option>
+            <option value="Контрольная">Контрольная</option>
+          </select>
+          <input type="text" name="img" placeholder="url изображения" />
+         {type != 'Тест' ? <input type="file" name="file"/> : <Test />} 
+          <input type="submit"/>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/test.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+
+function Test() {
+    const [tests, setTests] = useState([]);
+    return (
+        <>
+            <input type="text" placeholder="Название теста"/>
+            {
+                tests.map((item,index) => {
+                   const ind = index;
+                   return (<>
+                        вопрос {index + 1}
+                        <input type="text" value={item.title} onChange={(e) => setTests([...tests.slice(0, index),{
+                            title: e.target.value,
+                            choices: tests[index].choices,
+                            good: tests[index].good
+                        }, ...tests.slice(index + 1)])} placeholder={"Название вопроса"}/>
+                        {tests[index].choices.map((item,index) => {
+                            return (
+                                <>
+                                ответ {index + 1}
+                                <input type="text" value={item} onChange={(e) => {
+                                    setTests([...tests.slice(0, ind), {
+                                        title: tests[ind].title,
+                                        choices: [...tests[ind].choices.slice(0, index),e.target.value, ...tests[ind].choices.slice(index + 1)],
+                                        good: tests[ind].good
+                                    }, ...tests.slice(ind + 1)])
+                                }}/>
+                            </>
+                            )
+                         })}
+                         <button onClick={(e) => {
+                             e.preventDefault();
+                             setTests([...tests.slice(0, index), {
+                                title: tests[index].title,
+                                choices: [...tests[index].choices, ''],
+                                good: tests[index].good
+                             }, ...tests.slice(index+1)])
+                         }}>Добавить вариант</button>
+                    </>)
+                })
+            }
+            <button onClick={(e) => {
+                e.preventDefault();
+                setTests([...tests, {
+                    title: '',
+                    choices: [],
+                    good: 1
+                }])
+                console.log(tests);
+            }}>Добавить вопрос</button>
+        </>
+    )
+}
+
+export default Test;
